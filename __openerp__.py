787e097cb1f4728ad5b2201e92cee5bc71901951
Tony Galmiche <tony.galmiche@infosaone.com>
2014-10-05 10:40:59 +0200

    Mise en ligne

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+	"name" : "InfoSaône - Inventaire",
+	"version" : "0.1",
+	"author" : "InfoSaône",
+	"category" : "InfoSaône",
+
+
+   'description': """
+InfoSaône / Module permettant d'ajouter des fonctionnalés pour les inventaires : 
+- Ajout d'un menu pour importer lignes des inventaires
+
+===================================================
+""",
+    'maintainer': 'InfoSaône',
+    'website': 'http://www.infosaone.com',
+
+	"depends" : ["stock"],
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["is_inventory.xml"],
+	"installable": True,
+	"active": False
+}
+
+
+
+
